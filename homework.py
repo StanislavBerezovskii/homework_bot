6de487bfafeb22dfda09dade0f132b75af1c1b67
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-10-17 14:22:10 +0200

    Berezovskii_HWBot_2nd_fix_iteration

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 import time
 
 import requests
@@ -9,6 +10,8 @@ from http import HTTPStatus
 from dotenv import load_dotenv
 
 from exceptions import (ResponseError, StatusCodeError, TokenError)
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -31,8 +34,11 @@ TOKENS = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
 
 def send_message(bot, message):
     """Отправка сообщения об изменении статуса."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
-    logging.info(f'Отправлено сообщение: {message}')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info(f'Отправлено сообщение в Telegram: {message}')
+    except Exception:
+        logger.error('Сбой отправки сообщения в Telegram!')
 
 
 def get_api_answer(current_timestamp):
@@ -41,25 +47,33 @@ def get_api_answer(current_timestamp):
     try:
         response = requests.get(url=ENDPOINT, headers=HEADERS, params=params)
     except RequestException as error:
+        logger.error(f'Сервер yandex.practicum вернул ошибку: {error}')
         raise ConnectionError(f'Ошибка подключения к API: {error}')
     status_code = response.status_code
     if status_code != HTTPStatus.OK:
+        logger.error(f'Ошибка при запросе к API, код: {status_code}')
         raise StatusCodeError(f'Ошибка при запросе к API, код: {status_code}')
     try:
         response_json = response.json()
     except Exception as error:
-        raise ResponseError(f'Отказ обслуживания. Ошибка: {error}')
+        logger.error('Ответ от сервера должен быть в формате JSON')
+        raise ResponseError(f'Отказ работы сервера. Ошибка: {error}')
     return response_json
 
 
 def check_response(response):
     """Проверка ответа API."""
     if not isinstance(response, dict):
+        logger.error('Ответ API не является словарем!')
         raise TypeError('Ответ API не является словарем!')
     if 'homeworks' not in response:
+        logger.error('Отсутствует ключ "homeworks!"')
         raise KeyError('Отсутствует ключ "homeworks!"')
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
+        logger.error(
+            'Домашние задания оформлены не как список c ключом "homeworks"!'
+        )
         raise TypeError(
             'Домашние задания оформлены не как список c ключом "homeworks"!'
         )
@@ -69,18 +83,24 @@ def check_response(response):
 def parse_status(homework):
     """Извлекает из информации о домашней работе статус этой работы."""
     if 'status' not in homework:
+        logger.error(
+            'Отсутствует ключ "homework_name" в статусах работы'
+        )
         raise KeyError(
             'Отсутствует ключ "homework_name" в статусах работы'
         )
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     if homework_status not in VERDICTS:
+        logger.error(
+            f'Отсутствует ключ "{homework_status}" в статусах работы'
+        )
         raise KeyError(
             f'Отсутствует ключ "{homework_status}" в статусах работы'
         )
     verdict = VERDICTS[homework_status]
     if not verdict:
-        raise logging.error('Отсутствие ожидаемых ключей в ответе API.')
+        raise logger.error('Отсутствие ожидаемых ключей в ответе API.')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -89,7 +109,7 @@ def check_tokens():
     flag = True
     for name in TOKENS:
         if globals()[name] is None:
-            logging.critical(f'Токен {name} не найден!')
+            logger.critical(f'Токен {name} не найден!')
             flag = False
     return flag
 
@@ -109,19 +129,19 @@ def main():
             if homeworks:
                 send_message(bot, parse_status(homeworks[0]))
             else:
-                logging.info('Домашние задания не найдены.')
+                logger.info('Домашние задания не найдены.')
             current_timestamp = response.get(
                 'current_date',
                 current_timestamp
             )
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logging.critical(message)
-            if last_error != message:
-                try:
+            logger.critical(message)
+            try:
+                if last_error != message:
                     send_message(TELEGRAM_CHAT_ID, message)
-                except Exception as error:
-                    logging.exception(f'Сбой отправки сообщения: {error}')
+            except Exception as error:
+                logger.error(f'Сбой отправки сообщения: {error}')
                 last_error = message
         finally:
             time.sleep(RETRY_TIME)
@@ -136,4 +156,9 @@ if __name__ == '__main__':
         format=(
             '%(asctime)s, %(levelname)s, %(funcName)s, %(lineno)d, %(message)s'
         ))
-    main()
+    logger = logging.getLogger(__name__)
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.critical('Выход из программы по Ctrl-C')
+        sys.exit(0)
